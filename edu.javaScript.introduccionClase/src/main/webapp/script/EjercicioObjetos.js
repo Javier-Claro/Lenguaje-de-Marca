a1333d8009cc791b1b5751642f19d8227df96446
DESKTOP-LHCE2F2\csi22 <manuelclarobarba@gmail.com>
2023-01-24 12:59:42 +0100

    no message

---
+++ edu.javaScript.introduccionClase/src/main/webapp/script/EjercicioObjetos.js
@@ -0,0 +1,19 @@
+
+function ejercicio1() {
+	
+	var alumno = new Object();
+	
+	alumno.nombre = 'Javier';
+	alumno.apellidos = 'Claro barba';
+	alumno.edad = 20;	
+	
+    document.write("\n\n"+ typeof(alumno));
+    
+    document.write("\n\n"+ alumno.alumno);
+    
+    document.write("\n\n"+ alumno.nombre);
+    
+    document.write("\n\n"+ alumno.apellidos);
+    
+    document.write("\n\n"+ alumno.edad);
+}

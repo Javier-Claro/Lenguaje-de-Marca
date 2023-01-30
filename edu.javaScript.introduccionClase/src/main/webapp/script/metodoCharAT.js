35af8c5726fc8fef219b7415c2c0edbb485326e0
DESKTOP-LHCE2F2\csi22 <manuelclarobarba@gmail.com>
2023-01-30 10:08:44 +0100

    no message

--- edu.javaScript.introduccionClase/src/main/webapp/script/metodoCharAT.js
+++ edu.javaScript.introduccionClase/src/main/webapp/script/metodoCharAT.js
@@ -4,7 +4,7 @@ function ejemplo1() {
 	
 	document.write(unaCadena)
 	
-	document.write("\tEl carácter en el índice 3 es '" + unaCadena.charAt(3) + "'")	
-	document.write("\tEl carácter en el índice 7 es '" + unaCadena.charAt(7) + "'")
-	document.write("\tEl carácter en el índice 15 es '" + unaCadena.charAt(15) + "'")
+	document.write("<br>El carácter en el índice 3 es '" + unaCadena.charAt(3) + "'")	
+	document.write("<br>El carácter en el índice 7 es '" + unaCadena.charAt(7) + "'")
+	document.write("<br>El carácter en el índice 15 es '" + unaCadena.charAt(15) + "'")
 }

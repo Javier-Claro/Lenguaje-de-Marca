7f817bb2436f412c8b761ab04aa8d525accfaf64
DESKTOP-LHCE2F2\csi22 <manuelclarobarba@gmail.com>
2023-03-23 08:40:01 +0100

    no message

--- edu.javaScript.introduccionClase/src/main/webapp/script/alertFunction.js
+++ edu.javaScript.introduccionClase/src/main/webapp/script/alertFunction.js
@@ -2,3 +2,26 @@ function generarAlerta(){
     let num1 = 10;
     alert(num1);
 }
+
+function prueba1() {
+	let texto = "No soy un número";
+	let conversion= Number(texto);
+	document.write(typeof(conversion) + " " + conversion);
+}
+
+function prueba2() {
+	let numero1 = "3";
+	let numero2 = "7";
+	let numero3 = "10";
+	
+	let suma = Number(numero1) + Number(numero2) + Number(numero3);
+	
+	let concatenacion = numero1 + numero2 + numero3;
+	
+	document.write(suma);
+	document.write("	" + typeof(suma));
+	
+	document.write("\n" + concatenacion);
+	document.write("	" + typeof(concatenacion));
+	
+}
---
+++ edu.javaScript.introduccionClase/src/main/webapp/script/arrayEjemplo.js
@@ -0,0 +1,12 @@
+function ejemplo1() {
+	
+	let alumnos = ["Alfonso","Alberto","Carlos","José Maria"];
+
+	let solucion = alumnos[3].charAt(2);
+
+	document.write(solucion);
+}
+
+function ejemplo2() {
+	
+}
